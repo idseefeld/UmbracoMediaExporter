ccd9592f9acd4824aaee9eaab53292e6bfc59ce5
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2022-04-01 22:32:51 +0200

    update version for Umbraco 8

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Simple Media Exporter Solution for Umbraco 7
-Drop file ExportMedia.cs into your Umbraco webroot folder in App_Code (you may need to create this folder).
+## Umbraco 7
+Drop file v7\ExportMedia.cs into your Umbraco webroot folder in App_Code (you may need to create this folder).
 
 Edit line 22 of ExportMedia.cs: 
 ```
@@ -7,3 +8,13 @@ private readonly string _exportRoot = "c:\\UmbracoMediaExport";
 //the applicationPool user needs write permission for _exportRoot folder.
 ```
 Currently this solution is tested with Umbraco version 7.14.1 and 7.15.7.
+
+## Umbraco 8
+Drop file v8\ExportMediaV8.cs into your Umbraco webroot folder in App_Code (you may need to create this folder).
+
+Edit line 24 of ExportMediaV8.cs: 
+```
+private readonly string _exportRoot = "c:\\UmbracoMediaExport";
+//the applicationPool user needs write permission for _exportRoot folder.
+```
+Currently this solution is tested with Umbraco version 8.18.2.
--- v7/ExportMedia.cs
+++ v7/ExportMedia.cs
@@ -5,103 +5,90 @@ using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using Umbraco.Core;
-using Umbraco.Core.Composing;
-using Umbraco.Core.Logging;
-using Umbraco.Core.Models.PublishedContent;
-using Umbraco.Web;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
 
 namespace idseefeld.de
 {
     /// <summary>
-    /// ExportMediaService exports all Umbraco media items into an external folder structur with item names for folders and files 
+    /// ExportMedia exports all Umbraco media items into an external folder structur with item names for folders and files 
     /// as these are defined in Umbracos media section
     /// Put this file into [webroot]\App_Code folder.
     /// You might change the _exportRoot path to your needs. And you may set suitable permissions on that folder.
+    /// and put the following line into your master or homepage template:
+    /// @{ var mediaExporter = new idseefeld.de.ExportMedia(ApplicationContext); var mediaExporterResult = mediaExporter.Export(); }
+    /// You may display @mediaExporterResult in your html, but that is not necassary.
     /// </summary>
-    public class ExportMediaService
+    public class ExportMedia
     {
-        #region properties
-        private readonly string _exportRoot = "c:\\MediaExportUmbraco8";
+        private readonly string _exportRoot = "c:\\UmbracoMediaExport";
         //_exportRoot needs write permission for the applicationPool user.
 
         private readonly string _umbracoRoot;
-        private readonly ILogger _logger;
-        private readonly bool exportToEmptyFolderOnly = true;
-        private readonly UmbracoContext _umbracoContext;
-        private readonly char[] _invalidFilenameChars = Path.GetInvalidFileNameChars();
-
-        private readonly IUmbracoContextFactory _umbracoContextFactory;
-        #endregion
-
-        #region ctors
-        public ExportMediaService(UmbracoContext umbracoContext)
+        private readonly ApplicationContext _applicationContext;
+        private readonly char[] _invalidFilenameChars;
+        public ExportMedia(ApplicationContext applicationContext, Umbraco.Web.UmbracoContext umbracoContext)
         {
-            _umbracoContext = umbracoContext;
-            _umbracoRoot = _umbracoContext.HttpContext.Server.MapPath("/");
+            _applicationContext = applicationContext;
+            _invalidFilenameChars = Path.GetInvalidFileNameChars();
+            _umbracoRoot = umbracoContext.HttpContext.Server.MapPath("/");
         }
-        public ExportMediaService(IUmbracoContextFactory umbracoContextFactory, ILogger logger)
+        public ExportMedia(ApplicationContext applicationContext, string webRoot)
         {
-            _logger = logger;
-            _umbracoContextFactory = umbracoContextFactory;
-            using (var umbracoContextRef = _umbracoContextFactory.EnsureUmbracoContext())
-            {
-                _umbracoContext = umbracoContextRef.UmbracoContext;
-                _umbracoRoot = _umbracoContext.HttpContext.Server.MapPath("/");
-            }
+            _applicationContext = applicationContext;
+            _invalidFilenameChars = Path.GetInvalidFileNameChars();
+            _umbracoRoot = webRoot;
+            Export();
         }
-        #endregion
-
         public string Export()
         {
-            string resultMessage;
-            MediaFolderAndFileInfo exportStructur;
+            var rVal = "No Media Root found.";
+            MediaFolderAndFileInfo exportStructur = null;
             try
             {
                 var exportRoot = Directory.CreateDirectory(_exportRoot);
-                if (exportToEmptyFolderOnly && exportRoot.EnumerateFiles().Count() > 0)
-                {
-                    resultMessage = $"Media items allready exported. For new export delete all content of: {_exportRoot}";
-                    Logger(resultMessage);
-                    return resultMessage;
-                }
-                var mr = _umbracoContext.Media.GetAtRoot();
-                if (mr == null)
-                {
-                    resultMessage = "No Media Root found.";
-                    Logger(resultMessage);
-                    return resultMessage;
-                }
+                if (exportRoot.EnumerateFiles().Count() > 0) return $"Media items allready exported. For new export delete all content of: {_exportRoot}";
+
+
+                var ms = _applicationContext.Services.MediaService;
+                var mr = ms.GetRootMedia();
+                if (mr == null) return rVal;
 
                 var fixedNames = new List<FixedNames>();
                 exportStructur = new MediaFolderAndFileInfo()
                 {
                     Name = "Media",
                     PathSegment = "",
-                    Children = GetMediaItemsRecursive(mr, _exportRoot, fixedNames)
+                    Children = GetMediaItemsRecursive(mr, ms, _exportRoot, fixedNames)
                 };
 
                 var reportJson = Json.Encode(exportStructur);
-                File.WriteAllText(Path.Combine(_exportRoot, "export-report.json"), reportJson);
+                System.IO.File.WriteAllText(Path.Combine(_exportRoot, "export-report.json"), reportJson);
 
                 if (fixedNames.Count > 0)
                 {
                     reportJson = Json.Encode(fixedNames);
-                    File.WriteAllText(Path.Combine(_exportRoot, "export-fixednames.json"), reportJson);
+                    System.IO.File.WriteAllText(Path.Combine(_exportRoot, "export-fixednames.json"), reportJson);
                 }
 
-                resultMessage = "Media Section exported.";
-                Logger(resultMessage);
+                rVal = "Media Section exported.";
             }
             catch (Exception ex)
             {
-                Logger(ex);
-
-                resultMessage = $"Media Section not exported! {Environment.NewLine}{ex.Message} {Environment.NewLine}{ex.Source} {Environment.NewLine}{ex.StackTrace}";
-                System.IO.File.WriteAllText(Path.Combine(_exportRoot, "export-error.json"), resultMessage);
+                rVal = "Media Section not exported!";
+                System.IO.File.WriteAllText(
+                    Path.Combine(_exportRoot, "export-error.json"),
+                    $"{rVal} {System.Environment.NewLine}{ex.Message} {System.Environment.NewLine}{ex.Source} {System.Environment.NewLine}{ex.StackTrace}");
             }
-            return resultMessage;
+            return rVal;
         }
-        private IEnumerable<MediaFolderAndFileInfo> GetMediaItemsRecursive(IEnumerable<IPublishedContent> mr, string parentPath, List<FixedNames> fixedNames)
+
+        private IEnumerable<MediaFolderAndFileInfo> GetMediaItemsRecursive(
+            IEnumerable<IMedia> mr,
+            IMediaService ms,
+            string parentPath,
+            List<FixedNames> fixedNames,
+            MediaFolderAndFileInfo info = null)
         {
             var rVal = new List<MediaFolderAndFileInfo>();
 
@@ -127,8 +114,7 @@ namespace idseefeld.de
                 {
                     var umbracoFile = item.Properties
                         .FirstOrDefault(p => p.Alias == "umbracoFile")?
-                        .GetValue(umbracoFilePath)
-                        .ToString();
+                        .Value.ToString();
                     if (!string.IsNullOrEmpty(umbracoFile))
                     {
                         if (item.ContentType.Alias == "Image")
@@ -142,10 +128,7 @@ namespace idseefeld.de
                                     umbracoFile = cropperValue.Src;
                                 }
                             }
-                            catch (Exception ex)
-                            {
-                                if (_logger != null) _logger.Error<ExportMediaService>(ex);
-                            }
+                            catch { }
                         }
                         var relativePath = umbracoFile.Trim('/').Replace('/', '\\');
                         umbracoFilePath = Path.Combine(_umbracoRoot, relativePath);
@@ -172,11 +155,11 @@ namespace idseefeld.de
                 }
                 else
                 {
-                    if (File.Exists(umbracoFilePath))
+                    if (System.IO.File.Exists(umbracoFilePath))
                     {
-                        if (!File.Exists(exportPath))
+                        if (!System.IO.File.Exists(exportPath))
                         {
-                            File.Copy(umbracoFilePath, exportPath);
+                            System.IO.File.Copy(umbracoFilePath, exportPath);
                         }
                     }
                     else
@@ -192,13 +175,15 @@ namespace idseefeld.de
 
                 if (fixedFilenamesOrErrors != null) { fixedNames.Add(fixedFilenamesOrErrors); }
 
-                var children = item.Children.ToArray();
-                if (children.Length > 0)
+                var children = item.Children();
+                if (children != null)
                 {
                     newExportElement.Children = GetMediaItemsRecursive(
-                            children,
-                            exportPath,
-                            fixedNames);
+                        children,
+                        ms,
+                        exportPath,
+                        fixedNames,
+                        newExportElement);
                 }
                 rVal.Add(newExportElement);
             }
@@ -216,37 +201,23 @@ namespace idseefeld.de
             }
             return rVal;
         }
-        private void Logger(Exception ex = null)
-        {
-            Logger(null, ex);
-        }
-        private void Logger(string message = null, Exception ex = null)
-        {
-            if (_logger == null) return;
-
-            if (ex == null)
-            {
-                _logger.Debug<ExportMediaService>(message);
-            }
-            else
-            {
-                _logger.Error<ExportMediaService>(ex);
-            }
-        }
-
         #region helper classes
-
-        private class ImageCropperValue
+        public class ImageCropperValue
         {
             public string Src { get; set; }
             public FocalPoint FocalPoint { get; set; }
+            public IEnumerable<Crop> Crops { get; set; }
         }
-        private class FocalPoint
+        public class Crop
+        {
+            public string Name { get; set; }
+        }
+        public class FocalPoint
         {
             public float Left { get; set; }
             public float Top { get; set; }
         }
-        private class MediaFolderAndFileInfo
+        public class MediaFolderAndFileInfo
         {
             public int Id { get; set; }
             public string Name { get; set; }
@@ -257,7 +228,7 @@ namespace idseefeld.de
             public FocalPoint FocalPoint { get; set; }
             public IEnumerable<MediaFolderAndFileInfo> Children { get; set; }
         }
-        private class FixedNames
+        public class FixedNames
         {
             public string UmbracoName { get; set; }
             public string FixedName { get; set; }
@@ -266,51 +237,16 @@ namespace idseefeld.de
         #endregion
     }
 
-    #region register IComponent to execute ExportMediaService.Export() on HttpApplication.BeginRequest with IComposer
-
-    public class ExportMediaWhenAppStartedComposer : IComposer
+    /// <summary>
+    /// The following ApplicationEventHandler adds automatic execution of ExportMedia.Export() on application start.
+    /// Btw. the application starts when you change this file.
+    /// </summary>
+    public class ExportMediaWhenAppStarted : ApplicationEventHandler
     {
-        public void Compose(Composition composition)
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
-            composition.Components().Append<ExportMediaWhenAppStartedComponent>();
+            var webRoot = umbracoApplication.Server.MapPath("/");
+            var exporter = new ExportMedia(applicationContext, webRoot);
         }
     }
-
-    public class ExportMediaWhenAppStartedComponent : IComponent
-    {
-        private readonly ILogger _logger;
-        private readonly IUmbracoContextFactory _contextFactory;
-
-        public ExportMediaWhenAppStartedComponent(
-            IUmbracoContextFactory contextFactory,
-            ILogger logger)
-        {
-            _logger = logger;
-            _contextFactory = contextFactory;
-        }
-
-        public void Initialize()
-        {
-            UmbracoApplication.ApplicationInit += UmbracoApplication_ApplicationInit;
-        }
-
-        private void UmbracoApplication_ApplicationInit(object sender, EventArgs e)
-        {
-            if (!(sender is HttpApplication app)) return;
-
-            app.BeginRequest += App_BeginRequest;
-        }
-
-        private void App_BeginRequest(object sender, EventArgs e)
-        {
-            var exporter = new ExportMediaService(_contextFactory, _logger);
-            var result = exporter.Export();
-        }
-
-        public void Terminate()
-        {
-            UmbracoApplication.ApplicationInit -= UmbracoApplication_ApplicationInit;
-        }
-    }
-    #endregion
 }
